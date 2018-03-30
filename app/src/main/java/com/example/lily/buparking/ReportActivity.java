4065a5a0ebd24fda9c200135a95f37a864bdc1cb
xiuxiuxiu <glxlily@bu.edu>
2018-03-29 20:43:34 -0400

    Map Update
    Customized map along with submit

---
+++ app/src/main/java/com/example/lily/buparking/ReportActivity.java
@@ -0,0 +1,27 @@
+package com.example.lily.buparking;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ReportActivity extends AppCompatActivity {
+
+    private Button btn_submit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        btn_submit = findViewById(R.id.btn_submit);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
